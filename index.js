bc9434c92e81e196553b25eb1991d3a4d758cfdc
gauravshankarcan <gauravshankar.can@gmail.com>
2021-12-13 23:02:10 -0500

    parse complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
-const fastify = require('fastify')({ logger: {level: 'debug', prettyPrint: true} })
+const env = require('dotenv').config()
+var jp = require('jsonpath');
 
+const fastify = require('fastify')({ logger: {level: 'debug'} })
 
 // Declare a route
 fastify.get('/health', async (request, reply) => {
@@ -10,17 +11,35 @@ fastify.get('/health', async (request, reply) => {
 
 // Declare a route
 fastify.post('/', async (request, reply) => {
-    fastify.log.debug("Hook received")
-    fastify.log.debug(JSON.stringify(request.body, null, '\t'))
+    fastify.log.debug(JSON.stringify(request.body, null, '\t'));   
+    var response = await process(request.body)
+    //var createOrUpdate = await search(request.body,response.u_fingerprint)
+    fastify.log.debug(response);
+    return { request: response }
+})
 
-    
-    
-    return { request: true }
-  })
+async function process(body,fingerprint) {
+  
+  var search = await axios({
+                        method: 'get',
+                        url: env.parsed.OUTBOUND_URL+'?u_fingerprint='+fingerprint
+                      })           
 
+  return true;
+}
 
-
-
+async function process(body) {
+  var response = {};
+  var keylist = Object.keys(env.parsed)
+  for (var i = 0; i < keylist.length; i++) {
+      var validkey = keylist[i].split("ESSBODY_")
+      if(validkey[0]=="PROC") {
+        console.log(env.parsed[keylist[i]])
+        response[validkey[1].toLowerCase()]=jp.query(body,'$.'+env.parsed[keylist[i]])[0]
+      }
+  } 
+  return response 
+};
 
 
 
